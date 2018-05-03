0a36372833c481a94023143755f759d52da42c37
webcursesprogram@gmail.com <webcursesprogram@gmail.com>
2018-05-03 11:40:39 +0200

    Creados los objetos Personajes, Armas y Habitaciones

--- js/objects.js
+++ js/objects.js
@@ -1,66 +1,175 @@
-var mrGreen = {
-    first_name:   "Jacob",
-    last_name:    "Green",
-    color:        "green",
-    description:  "He has a lot of connections",
-    age:          45,
-    image:        "http://img.charahub.com/48207fb80fcb4b2d8a4489d2edbdcc90.png",
-    occupation:   "Entrepreneur"
-}
+// Characters
+var charactersArray = [
+    mrGreen = {
+        first_name:   "Jacob",
+        last_name:    "Green",
+        color:        "green",
+        description:  "He has a lot of connections",
+        age:          45,
+        image:        "http://img.charahub.com/48207fb80fcb4b2d8a4489d2edbdcc90.png",
+        occupation:   "Entrepreneur"
+    },
 
-var drOrchid = {
-    frst_name:   "Doctor",
-    lst_name:    "Orchid",
-    color:        "white",
-    description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-    age:          26,
-    image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-    ocupation:   "Scientist"
-}
+    drOrchid = {
+        frst_name:   "Doctor",
+        lst_name:    "Orchid",
+        color:        "white",
+        description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        age:          26,
+        image:        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        ocupation:   "Scientist"
+    },
 
-var profPlum = {
-    first_name:   "Victor",
-    last_name:    "Plum",
-    color:        "purple",
-    description:  "Billionare video game designer",
-    age:          22,
-    image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
-    occupation:   "Designer"
-}
+    profPlum = {
+        first_name:   "Victor",
+        last_name:    "Plum",
+        color:        "purple",
+        description:  "Billionare video game designer",
+        age:          22,
+        image:        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+        occupation:   "Designer"
+    },
 
-var missScarlet = {
-    first_name:   "Kasandra",
-    last_name:    "Scarlet",
-    color:        "red",
-    description:  "She is an A-list movie star with a dark past",
-    age:          31,
-    image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
-    occupation:   "Actor"
-}
+    missScarlet = {
+        first_name:   "Kasandra",
+        last_name:    "Scarlet",
+        color:        "red",
+        description:  "She is an A-list movie star with a dark past",
+        age:          31,
+        image:        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+        occupation:   "Actor"
+    },
 
-var mrsPeacock = {
-    first_name:   "Eleanor",
-    last_name:    "Peacock",
-    color:        "blue",
-    description:  "She is from a wealthy family and uses her status and money to earn popularity",
-    age:          36,
-    image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-    occupation:   "Socialité"
-}
+    mrsPeacock = {
+        first_name:   "Eleanor",
+        last_name:    "Peacock",
+        color:        "blue",
+        description:  "She is from a wealthy family and uses her status and money to earn popularity",
+        age:          36,
+        image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        occupation:   "Socialité"
+    },
 
-var mrMustard = {
-    first_name:   "Jack",
-    last_name:    "Mustard",
-    color:        "yellow",
-    description:  "He is a former football player who tries to get by on his former glory",
-    age:          62,
-    image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
-    occupation:   "Retired Football player",
-}
+    mrMustard = {
+        first_name:   "Jack",
+        last_name:    "Mustard",
+        color:        "yellow",
+        description:  "He is a former football player who tries to get by on his former glory",
+        age:          62,
+        image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+        occupation:   "Retired Football player",
+    }
+];
+
+// Weapons
+var weaponsArray = [
+    rope = { 
+        name: "rope",
+        weight: 10
+    },
+
+    knife = {
+        name: "knife",
+        weight: 8
+    },
+
+    candlestick = {
+        name: "candlestick",
+        weight: 2
+    },
+
+    dumbbell = {
+        name: "dumbbell",
+        weight: 30
+    },
+
+    poison = {
+        name: "poison",
+        weight: 2
+    },
+
+    axe = {
+        name: "axe",
+        weight: 15
+    },
+
+    bat = {
+        name: "bat",
+        weight: 13
+    },
 
 
-var charactersArray = [];
+    trophy = {
+        name: "trophy",
+        weight: 25
+    },
 
-var roomsArray = [];
+    pistol = {
+        name: "pistol",
+        weight: 20
+    }
+];
 
-var weaponsArray = [];
+// Rooms
+var roomsArray = [
+    Dinning_Room = {
+        name: "Dinning Room"
+    },
+
+    Conservatory = {
+        name: "Conservatory"
+    },
+
+    Kitchen = {
+        name: "Kitchen"
+    },
+
+    Study = {
+        name: "Study"
+    },
+
+    Library = {
+        name: "Library"
+    },
+
+    Billiard_Room = {
+        name: "Billiard Room"
+    },
+
+    Lounge = {
+        name: "Lounge"
+    },
+
+    Ballroom = {
+        name: "Ballroom"
+    },
+
+    Hall = {
+        name: "Hall"
+    },
+
+    Spa = {
+        name: "Spa"
+    },
+
+    Living_Room = {
+        name: "Living Room"
+    },
+
+    bservatory = {
+        name: "Observatory"
+    },
+
+    Theater = {
+        name: "Theater"
+    },
+
+
+    Guest_House = {
+        name: "Guest House"
+    },
+
+    Patio = {
+        name: "Patio"
+    }
+];
